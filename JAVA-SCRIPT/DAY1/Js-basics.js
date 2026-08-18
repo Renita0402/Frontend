64df8a210748e8b228e898374b3861c9a764bd19
Renita <renitar945@gmail.com>
2026-08-18 17:10:59 +0530

    updated

---
+++ JAVA-SCRIPT/DAY1/Js-basics.js
@@ -0,0 +1,274 @@
+VAR
+
+1.var bluePen
+2.var blackPen
+3.var redPen
+4.var greenPen
+5.var noteBook
+6.var whitePage
+7.var ruledPage
+8.var ledPencil
+9.var whitePage
+10.var schoolbag
+11.var lunchBag
+12.var sliverSpoon
+13.var waterBottle
+14.var lock-Key
+15.var pantShirt
+16.var idCard
+17.var milkSweet
+18.var nailPolish
+19.var sunscreen
+20.var lipGloss
+21.var lipstick
+22.var curdRice
+23.var carWheel
+24.var greenPlant
+25.var surfExcel
+26.var dataAnalytics
+27.var phoneCase
+28.var javaScript
+29.var vegSalad
+30.var popCorn
+31.var breakPoint
+32.var smartWatch
+33.var androidTV
+34.var onePlus
+35.var headPhone
+36.var watermelon
+37.var butterSctoch
+38.var iceCream
+39.var playStore
+40.var mildAttack
+41.var bloodPressure
+42.var waterPressure
+43.var powerPoint
+44.var sunRays
+45.var playDay
+46.var themeStore
+47.var clayMask
+48.var pointOut
+
+LET
+
+1.let tubeLight
+2.let switchbox
+3.let cottonCandy
+4.let tableMate
+5.let screwDriver
+6.let googlePhoto
+7.let youTube
+8.let spiderMan
+9.let blackBoard
+10.let classRoom
+11.let vacumTube
+12.let snap-Chat
+13.let whats-App
+14.let hot-Box
+15.let key-Board
+16.let goldChain
+17.let goldRing
+18.let gold-Anklet
+19.let sliverBraclet
+20.let pencilBox
+21.let socialScience
+22.let giftWrap
+23.let basketBall
+24.let vollyBall
+25.let timeLimit
+26.let easyBuy
+27.let tapWater
+28.let saltWater
+29.let cupBoard
+30.let examPad
+31.let questionPaper
+32.let notePad
+33.let visualStudio
+34.let redGaint
+35.let carryBag
+36.let windMill
+37.let solarPannel
+38.let sunRise
+39.let airCooler
+40.let hairDryer
+41.let broomStick
+42.let mopStick
+43.let roomSpray
+44.let handWash
+45.let cloudComputing
+46.let dataScience
+47.let dataStorage
+48.let dataBase
+
+Const
+
+1.const userName
+2.const userEmail
+3.const userAge
+4.const userForm
+5.const userLogin
+6.const forgotPassword
+7.const userName-Password
+8.const blindFloder
+9.const wallPaper
+10.const  deepClean
+11.const dataUsage
+12.const dataUnlimitted
+13.const microMax
+14.const sunFlower
+15.const phoneHolder
+16.const newGames
+17.const photoShop
+18.const darkNight
+19.const sunnyMorning
+20.const goodDay
+21.const callCenter
+22.const hyperLink
+23.const hyperText
+24.const openEditor
+25.const spaceBar
+26.const openFolder
+27.const blueLight
+28.const colorPen
+29.const headLight
+30.const temperGlass
+31.const smartProjector
+32.const redSky
+33.const boxOffice
+34.const cinimaTime
+35.const ticketCounter
+36.const bookTicket
+37.const playGround
+38.const waterMarker
+39.const workFlow
+40.const kineticEnergy
+41.const variableTypes
+42.const dataTypes
+43.const variableDeclaration
+44.const whiteBoard
+45.const lightHouse
+46.const checkList
+47.const waterPaint
+48.const programingLanguage
+
+INTERVIEW QUESTIONS & ANSWERS
+
+1.What is JavaScript?
+
+JavaScript is a programing language used make website interactive dynamic and intelligent
+It is a high level language
+It is interpreted language
+Dynamic typed
+
+2.What is a variable?
+
+ A variable has a name and you can stored something in it
+
+3.What is variable declaration?
+
+Declaration means creating a variable without assigning a value
+
+Eg: var userName 
+
+var = keyword
+userName = variable declaration
+
+This is called variable declaration
+
+4.What is initialization?
+
+  Initialization means assigning an initial value to a variable
+
+Eg: var username = "React";
+
+var  keyword
+userName  variable declaration
+= assignment operator
+React  value
+
+5.What is assignment operator?
+
+ The assignment operator is : =
+ It is used assing a value to variable
+
+Eg: var username = "React";
+
+var  keyword
+userName  variable declaration
+= assignment operator
+React  value
+
+6.What are var let and const?
+
+1. var
+Older way of declaring variables.
+var is function-scoped, not block-scoped.
+ 
+Eg: var username = "React";
+
+2. let
+Used when the value needs to be changed.
+But you cannot redeclare it in the same scope
+
+Eg: let age = 25;
+      age = 30;   
+
+3. const
+Used when the variable should not be reassigned.
+A const variable must be initialized when declared.
+
+Eg: const name = "John";
+
+
+7.What is the difference between var, let and const?
+
+var
+Function scoped
+Can be redeclared
+Gets hoisted (but initialized as undefined)
+Not used in modern JS anymore
+
+let
+Block scoped
+Can be updated but not redeclared
+Not hoisted (in the same way)
+Safer than var
+
+const
+Block scoped
+Cannot be updated or redeclared
+Ideal for constants or functions you don’t want reassigned
+
+
+8.What is ES6?
+
+ ES6 stands for ECMAScript 2015.
+ It is the 6th major version of the ECMAScript standard for JavaScript, released in 2015.
+ ES6 introduced many important features that made JavaScript more powerful, readable, and easier to write.
+
+
+9.What is javascript engine?
+
+ The browser contain a Js engine
+The Js engine m is a responsible for understanding and excecuting Js code 
+
+Eg:
+Chrome = V8Engine
+Edge = V8Engine
+Firerfox = SpiderMonkey
+safari = Js code
+
+10.What is v8 engine
+
+ V8 engine is google's Js engine
+ Chrome uses the v8 engine to excecute javascript
+
+process
+
+Js code
+    |
+v8 Engine
+    |
+Excecution
+   |
+Resut
