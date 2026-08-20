ca66874fbf5d2124139524ac136d08335f231d6e
Renita <renitar945@gmail.com>
2026-08-20 14:42:39 +0530

    Task update

---
+++ JAVA-SCRIPT/DAY3/Practiice/assets/js/index.js
@@ -0,0 +1,59 @@
+let userName = "Renita"
+userName = "Renisha"
+console.log(userName);
+
+let userPassword = "your name"
+userPassword = "reni@230"
+console.log(userPassword);
+
+let emailAddress = "renita@gmail.com"
+ emailAddress = "renita.official@gmail.com"
+console.log(emailAddress);
+
+let emailPassword = "renita@29983"
+emailPassword = "ren@20202"
+console.log(emailPassword);
+
+let studantName = "Sheeba"
+studentName = "SheebaJose"
+console.log (studentName);
+
+let studentReg = 2223056
+studentReg = 2223057
+console.log (studentReg);
+
+let employeeName = "Rajkumar"
+employeeName = "Raj"
+console.log (employeeName);
+
+let currentStatus = "Activated"
+currentStatus = "Unactivated"
+console.log (currentStatus);
+
+let studentMark = 50
+studentMark = 70
+console.log(studentMark);
+
+let totalMarks = 50
+totalMarks += 10
+console.log(totalMarks);
+
+let totalCount = 50
+totalCount -= 10
+console.log(totalCount);
+
+let totalStudent = 20
+totalStudent *= 30
+console.log(totalStudent);
+
+let totalAmount = 2000
+totalAmount /= 4 
+console.log(totalAmount);
+
+
+
+
+
+
+
+
---
+++ JAVA-SCRIPT/DAY3/TASK2/assets/js/index.js
@@ -0,0 +1,16 @@
+let score = 100;
+score += 20;
+console.log(score);
+
+score = 120;
+score -= 30;
+console.log(score);
+
+Score = 90;
+score *= 2;
+console.log(score);
+
+score = 180;
+score /= 3;
+console.log(score);
+
