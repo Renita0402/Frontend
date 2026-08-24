b1ecbf9775c479d4d60009c0d2cd7f2acb90791e
Renita <renitar945@gmail.com>
2026-08-24 13:05:56 +0530

    Task Update

---
+++ JAVA-SCRIPT/DAY5/ASSINGNMENT/TASK6/assets/js/index.js
@@ -0,0 +1,208 @@
+for (let i = 1; i <= 1; i++) {
+    console.log(i);
+}
+
+
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 9; i++) {
+    console.log(i);
+}
+
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+
+for (let i = 2; i <= 18; i += 2) {
+    console.log(i);
+}
+
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 20; i += 2) {
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 4; i <= 12; i++) {
+    console.log(i);
+}
+
+
+for (let i = 10; i >= 5; i--) {
+    console.log(i);
+}
+
+
+for (let i = 11; i <= 15; i++) {
+    console.log(i);
+}
+
+
+for (let i = 7; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 6; i <= 11; i++) {
+    console.log(i);
+}
+
+
+for (let i = 10; i <= 15; i++) {
+    console.log(i);
+}
+
+
+for (let i = 15; i <= 16; i++) {
+    console.log(i);
+}
+
+
+for (let i = 8; i <= 13; i++) {
+    console.log(i);
+}
+
+
+for (let i = 2; i <= 10; i++) {
+    console.log(i);
+}
+
+
+for (let i = 4; i <= 8; i++) {
+    console.log(i);
+}
+
+
+for (let i = 13; i <= 15; i++) {
+    console.log(i);
+}
+
+
+for (let i = 2; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 5; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 3; i <= 15; i++) {
+    console.log(i);
+}
+
+
+for (let i = 6; i <= 15; i++) {
+    console.log(i);
+}
+
+
+for (let i = 9; i <= 20; i++) {
+    console.log(i);
+}
+
+
+for (let i = 13; i <= 20; i++) {
+    console.log(i);
+}
+
+
+for (let i = 20; i <= 27; i++) {
+    console.log(i);
+}
+
+
+for (let i = 19; i <= 25; i++) {
+    console.log(i);
+}
+
+
+for (let i = 15; i <= 25; i++) {
+    console.log(i);
+}
+
+for (let i = 23; i <= 25; i++) {
+    console.log(i);
+}
+
+
+for (let i = 7; i <= 35; i++) {
+    console.log(i);
+}
+
+
+for (let i = 4; i <= 30; i++) {
+    console.log(i);
+}
+
+for (let i = 2; i <= 8; i++) {
+    console.log(i);
+}
+
+
+for (let i = 4; i <= 12; i++) {
+    console.log(i);
+}
+
+
+for (let i = 10; i <= 24; i++) {
+    console.log(i);
+}
+
+for (let i = 13; i <= 26; i++) {
+    console.log(i);
+}
+
+
+for (let i = 20; i <= 24; i++) {
+    console.log(i);
+}
+
+for (let i = 22; i <= 30; i++) {
+    console.log(i);
+}
+
+for (let i = 25; i <= 35; i++) {
+    console.log(i);
+}
+
+for (let i = 30; i <= 40; i++) {
+    console.log(i);
+}
+
+for (let i = 15; i <= 30; i++) {
+    console.log(i);
+}
+
+// 42
+for (let i = 19; i <= 28; i++) {
+    console.log(i);
+}
+
+for (let i = 18; i <= 28; i++) {
+    console.log(i);
+}
+for (let i = 13; i <= 25; i++) {
+    console.log(i);
+}
+
+for (let i = 11; i <= 20; i++) {
+    console.log(i);
+}
